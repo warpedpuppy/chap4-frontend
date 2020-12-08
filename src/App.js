393454c03efc13bc5f217b793ba26229eef80126
ted <ted@warpedpuppy.com>
2020-12-08 11:17:32 -0500

    add router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Meet from './components/Meet';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>header</h1>
       </header>
+      <main>
+        <Switch>
+          <Route exact path={'/'} component={Home} />
+          <Route path={'/meet'} component={Meet} />
+        </Switch>
+      </main>
     </div>
   );
 }
---
+++ src/components/Meet.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Meet extends Component {
+    render() {
+        return (
+            <div>
+                <h1>meet</h1>
+            </div>
+        )
+    }
+}
